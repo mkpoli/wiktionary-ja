2fd1e810feff9ec9996ffae42e3d521b811d241b
mkpoli <git@mkpo.li>
2024-11-08 16:27:03 +0900

    feat: export all main classes

--- wikitext/src/wikitext/__init__.py
+++ wikitext/src/wikitext/__init__.py
@@ -1,4 +1,20 @@
 from .template import Template
 from .link import Link
+from .richtext import Bold, Italic
+from .heading import Heading
+from .list import OrderedList, UnorderedList
+from .citation import Citation, CiteBook, Ref
 
-__all__ = ["Template", "Link"]
+
+__all__ = [
+    "Template",
+    "Link",
+    "Bold",
+    "Italic",
+    "Heading",
+    "OrderedList",
+    "UnorderedList",
+    "Citation",
+    "CiteBook",
+    "Ref",
+]
