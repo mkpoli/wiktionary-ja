93538003b7fdc0c35946bbc0103571161b11a204
mkpoli <git@mkpo.li>
2024-11-08 21:48:10 +0900

    test: add furigana test

---
+++ packages/wiktionary/tests/jawikt/test_furigana.py
@@ -0,0 +1,20 @@
+from jawikt.furigana import ふりがな, おくりがな2, おくりがな3
+
+
+def test_furigana():
+    assert ふりがな("漢字", "かんじ") == "{{ふりがな|漢字|かんじ}}"
+
+
+def test_okurigana2():
+    # Test with the example from the comment: 辞める|や|める|やめる
+    assert (
+        おくりがな2("辞", "や", "める", "やめる") == "{{おくりがな2|辞|や|める|やめる}}"
+    )
+
+
+def test_okurigana3():
+    # Test with the example from the comment: 思|おも|い|遣|や|り|おもいやり
+    assert (
+        おくりがな3("思", "おも", "い", "遣", "や", "り", "おもいやり")
+        == "{{おくりがな3|思|おも|い|遣|や|り|おもいやり}}"
+    )
